ae5dbb39d9f0df181e648004e4f88ebf78902731
Gianluca Fioletti <gianluca.fioletti@smeup.com>
2018-11-23 10:27:38 +0100

    Some examples

---
+++ src/main/java/com/smeup/custom/component/JSFActivator.java
@@ -0,0 +1,10 @@
+package com.smeup.custom.component;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class JSFActivator {
+
+}
